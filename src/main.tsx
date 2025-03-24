fc60951f5fe025db9fad38bd9201ef47ff262de5
Holl4444 <holly.cello@gmail.com>
2025-03-24 02:31:33 +0000

    Fix vercel

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import ReactDOM from 'react-dom';
 import App from './App.tsx';
 import './index.module.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
 
 //embeds widget when called by the one liner!
 window.initWeatherWidget = function (config: {
@@ -18,20 +17,22 @@ window.initWeatherWidget = function (config: {
     typeof config.container === 'string'
       ? document.getElementById(config.container)
       : config.container;
-  
+
   if (container) {
-     try {
-       const root = createRoot(container);
-       root.render(<App />);
-     } catch (error) {
-       console.error(error);
-     }
+    try {
+      const root = createRoot(container);
+      root.render(<App />);
+    } catch (error) {
+      console.error(error);
+    }
   }
 };
 
 // TypeScript declaration: Typescript doesn't know about properties added to global objects, like window, unless you declare them. This is after the embed call as it is only used by Typescript at compile time. Hoisted.
-declare global { 
+declare global {
   interface Window {
-    initWeatherWidget: (config: { container: Element | string }) => void;
+    initWeatherWidget: (config: {
+      container: Element | string;
+    }) => void;
   }
 }
