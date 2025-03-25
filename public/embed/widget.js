2114348b6dceead8c257a43120edf2471e6fea42
Holl4444 <holly.cello@gmail.com>
2025-03-25 16:45:57 +0000

    update: script src

--- public/embed/widget.js
+++ public/embed/widget.js
@@ -27,7 +27,7 @@
       const bundleScript = document.createElement('script');
       bundleScript.type = 'text/javascript';
       bundleScript.src =
-        'https://weather-widget-pied.vercel.app/assets/index-DVuy20L3.js';
+        'https://weather-widget-pied.vercel.app/assets/index-aiC3vXYw.js';
 
       bundleScript.onload = function () {
         if (window.initWeatherWidget) {
