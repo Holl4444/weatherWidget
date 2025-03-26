530dd4cc9574dade4b1b8ad7c128af4b3bb42c45
Holl4444 <holly.cello@gmail.com>
2025-03-26 00:35:30 +0000

    update: script src

--- public/embed/widget.js
+++ public/embed/widget.js
@@ -27,7 +27,7 @@
       const bundleScript = document.createElement('script');
       bundleScript.type = 'text/javascript';
       bundleScript.src =
-        'https://weather-widget-one-roan.vercel.app/assets/index-DV-pQB5N.js';
+        'https://weather-widget-one-roan.vercel.app/assets/index-B-lv1GIx.js';
 
       bundleScript.onload = function () {
         if (window.initWeatherWidget) {
